e68631b24664cf01f3c80784d7b0af313d2f94e9
Krzysztof Kraj <kraj.m.krzysztof@gmail.com>
2025-01-16 21:12:57 +0100

    Fetching accounts from mocha

--- test/Inbox.test.js
+++ test/Inbox.test.js
@@ -5,25 +5,38 @@ const { Web3 } = require('web3');
 // Create an instance of web3 and connect to local test network
 const web3 = new Web3(ganache.provider());
 
-class Car {
-  park() {
-    return 'stopped';
-  }
-
-  drive() {
-    return 'vroom';
-  }
-}
-
-let car;
-beforeEach(() => car = new Car())
-
-describe('Car', () => {
-  it('can park', () => {
-    assert.equal(car.park(), 'stopped');
-  });
-
-  it('can drive', () => {
-    assert.equal(car.drive(), 'vroom');
-  });
+beforeEach(() => {
+  //  Get list of all accounts
+  // on web3 select ethereum network and call getAccounts method
+  web3.eth.getAccounts()
+    .then(fetchedAccounts => {
+      console.log(fetchedAccounts);
+    })
+  // Use one of these accounts to deploy the contract
 })
+
+describe('Inbox', () => {
+  it('deploys a contract', () => {})
+})
+// class Car {
+//   park() {
+//     return 'stopped';
+//   }
+
+//   drive() {
+//     return 'vroom';
+//   }
+// }
+
+// let car;
+// beforeEach(() => car = new Car())
+
+// describe('Car', () => {
+//   it('can park', () => {
+//     assert.equal(car.park(), 'stopped');
+//   });
+
+//   it('can drive', () => {
+//     assert.equal(car.drive(), 'vroom');
+//   });
+// })
