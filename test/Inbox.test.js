3d710ab57ab9f8ec9a3153486caf48f677d0d2f9
Krzysztof Kraj <kraj.m.krzysztof@gmail.com>
2025-01-16 21:38:38 +0100

    Deployed contract in test environment

--- test/Inbox.test.js
+++ test/Inbox.test.js
@@ -1,22 +1,27 @@
 const assert = require('assert');
 const ganache = require('ganache');
 const { Web3 } = require('web3');
-
 // Create an instance of web3 and connect to local test network
 const web3 = new Web3(ganache.provider());
+const { interface, bytecode } = require('../compile');
 
-beforeEach(() => {
+let accounts;
+let inbox;
+beforeEach(async () => {
   //  Get list of all accounts
   // on web3 select ethereum network and call getAccounts method
-  web3.eth.getAccounts()
-    .then(fetchedAccounts => {
-      console.log(fetchedAccounts);
-    })
+  accounts = await web3.eth.getAccounts();
+
   // Use one of these accounts to deploy the contract
+  inbox = await new web3. eth.Contract(JSON.parse(interface))
+    .deploy({ data: bytecode, arguments: ['Hello world!']})
+    .send({ from: accounts[0], gas: '1000000' });
 })
 
 describe('Inbox', () => {
-  it('deploys a contract', () => {})
+  it('deploys a contract', () => {
+    console.log(inbox);
+  })
 })
 // class Car {
 //   park() {
