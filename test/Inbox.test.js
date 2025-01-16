090d575f28f6f933d1089aef8ba5fba6e0929370
Krzysztof Kraj <kraj.m.krzysztof@gmail.com>
2025-01-16 20:53:34 +0100

    Mocha testing implementation

--- test/Inbox.test.js
+++ test/Inbox.test.js
@@ -1,6 +1,29 @@
+const assert = require('assert');
 const ganache = require('ganache');
 const { Web3 } = require('web3');
 
-// updated ganache and web3 imports added for convenience
+// Create an instance of web3 and connect to local test network
+const web3 = new Web3(ganache.provider());
 
-// contract test code will go here
+class Car {
+  park() {
+    return 'stopped';
+  }
+
+  drive() {
+    return 'vroom';
+  }
+}
+
+
+describe('Car', () => {
+  it('can park', () => {
+    const car = new Car();
+    assert.equal(car.park(), 'stopped');
+  });
+
+  it('can drive', () => {
+    const car = new Car();
+    assert.equal(car.drive(), 'vroom');
+  });
+})
